e787c4582120ab72dd82e92da32f1a84bfe4f4bc
Rahul T V <61172472+rahulram21@users.noreply.github.com>
2024-05-17 05:29:19 +0000

    Solved the Hindex problem

--- Try.cpp
+++ Try.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include <vector>
-using namespace std;
+#include <algorithm>
+using namespace std; 
+//maximum value of h such that the given researcher has published at least h papers that have each been cited at least h times.
 
 int main(){
-    vector<int> cit = {1,3,1};
-    int hIndex=cit[0];
+    vector<int> cit = {0};    //1  ,,,  3,0,6,1,5   0,1,3,5,6  1,1,3
+    int hIndex = cit.size();
+
+    sort(cit.begin(), cit.end());
     for(int i=0; i<cit.size(); i++){
-        if(cit[i]>hIndex && cit[i] < cit.size()){
-            hIndex = cit[i];
-        }else{
-            while(hIndex > 0){
-                
-            }
+        if(cit[i] < hIndex){
+            hIndex--;
         }
     }
     cout<<"H index = "<<hIndex;
