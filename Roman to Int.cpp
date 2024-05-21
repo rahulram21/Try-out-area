cb705729f5494cda96e321a1889aa5a814f3317b
Rahul T V <61172472+rahulram21@users.noreply.github.com>
2024-05-21 05:39:13 +0000

    Roman to Int solved

--- Roman to Int.cpp
+++ Roman to Int.cpp
@@ -4,15 +4,23 @@
 
 using namespace std;
 
-int roman(string s){ //IV
+int roman(string s){ //MCMXCIV - 1994  
     int num = 0;
     map<char, int> mp;
     mp = {{'I',1}, {'V',5}, {'X',10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
 
     for(int i=0; i<s.length(); i++){
-        
+        if(mp[s[i]] < mp[s[i+1]]){
+            num = num-mp[s[i]];
+        }else{
+            num += mp[s[i]]; //104+
+        }
     }
+    return num;
 }
 int main(){
-
+   string s = "LVIII";
+   cout<<roman(s);
 }
+
+// Problem solved
