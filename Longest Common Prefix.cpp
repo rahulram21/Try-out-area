1d438babd66a6a7edceefb0e5161defc9d0b79fd
Rahul T V <61172472+rahulram21@users.noreply.github.com>
2024-06-09 02:32:45 +0000

    Completed problems for sample test set

--- Longest Common Prefix.cpp
+++ Longest Common Prefix.cpp
@@ -9,26 +9,30 @@
 using namespace std;
 
 int main(){
-    string strs[3] = {"flower","flow","flight"};
-    int first_subIndex = 0;
-    int last_subIndex = 2;
-    string prefix = strs[0].substr(first_subIndex, last_subIndex);
-    vector<string> prefixes;
-    bool available = false;
-    while(last_subIndex != strs[0].length()-1){
-        for(int i=1; i<3; i++){
-            if(strs[i].find(prefix)){
-                available = true;
-            }else{
-                first_subIndex++;
+    string strs[3] = {"dog","racecar","car"};
+
+    string prefix = strs[0]; //flower
+
+    for(int i=1; i<3; i++){
+        int minLength = min(prefix.length(), strs[i].length());
+        for(int j=0; j < minLength; j++){
+            if(prefix[j] != strs[i][j]){             
+                prefix = prefix.substr(0, j);
             }
-            if(i==2 && strs[i].find(prefix)){
-                prefixes.push_back(prefix);
-                last_subIndex++;
-            }
-        }
+        } 
     }
-    for(const auto& val: prefixes){
-        cout<<val<<endl;
-    }
+
+    cout<<prefix<<endl;
+    
+    
 }
+
+// found solution but has heap overflow error in leetcode
+
+//Another algorithm
+// find the smallest string. Compare the characters in this string with other strings.
+// Trim out the characters which are not in the smallest string.
+// Determine which is the smallest string.
+// Follow step 1-3
+// Exit once all strings are equal.
+// {"flower","flow","flight"} -> {"flow","flow","fl"} -> {"fl","fl","fl"}
