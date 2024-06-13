b27c621cf22c237322f1594144480fd388b3ea61
Rahul T V <61172472+rahulram21@users.noreply.github.com>
2024-06-13 05:23:12 +0000

    5 important directives

--- Angular/my-app/src/app/app.component.ts
+++ Angular/my-app/src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { ApiService } from './api.service';
 import { Observable } from 'rxjs';
 import { NgFor } from '@angular/common';
+import { NgClass } from '@angular/common';
+import { NgStyle } from '@angular/common';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +16,28 @@ export class AppComponent {
   userObservable$: Observable<any[]> = new Observable<any[]>
 
   constructor(private service:ApiService){}
+
+  setColor = false;
+  setColor2 = false;
+  currentColorStyle : Record<string, string> = {};
+  isColorChanged : boolean = false;
+
+  changeColor(){
+    if(this.isColorChanged){
+      this.currentColorStyle = {
+        'font-style' : 'normal',
+        'color' : 'initial'
+      };
+      this.setColor2 = false;
+    }else{
+      this.currentColorStyle = {
+        'font-style' : 'italic',
+        'color' : '#b8860b'
+      };
+      this.setColor2 = true;
+    }
+    this.isColorChanged = !this.isColorChanged;
+  }
 
   getUsers(){
     this.userObservable$ = this.service.getUser();
