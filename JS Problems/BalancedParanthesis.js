566f36284818b05bad1d23b4ed285f50da30f5c5
Rahul T V <61172472+rahulram21@users.noreply.github.com>
2024-07-20 02:55:13 +0000

    Balanced paranthesis done

---
+++ JS Problems/BalancedParanthesis.js
@@ -0,0 +1,20 @@
+
+const str = '(a + b) *  (c - d)';
+
+function checkBalance(str){
+    let arr = str.split('');
+    let left=0, right=0;
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] === ')'){
+            left +=1
+        }else if(arr[i] === '('){
+            right +=1
+        }
+    }
+    if(left === right){
+        return 'balanced'
+    }
+    return 'Not balanced'
+}
+
+console.log(checkBalance(str))
