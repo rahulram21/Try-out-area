04b16bb925f8deaa141c3c3c0455229ce0cb7447
Rahul T V <61172472+rahulram21@users.noreply.github.com>
2024-08-11 10:24:17 +0000

    function chaining using this pointer

--- C++/thisPointer.cpp
+++ C++/thisPointer.cpp
@@ -4,10 +4,21 @@ using namespace std;
 class Cars{
     public:
     int tires;
+    string make;
     int sumOfTires(int tires){
         this->tires = tires;
         return tires+1;
         // this pointer points the member function to the current instance of the class
+    }
+    
+    //this pointer also enables function chaining by pointing to the current object
+    Cars setTire(int tires){
+        this->tires = tires;
+        return *this;
+    }
+    Cars setMake(string make){
+        this->make = make;
+        return *this;
     }
 
 };
@@ -15,5 +26,7 @@ class Cars{
 int main(){
     Cars car1;
     Cars car2;
-    cout<<car1.sumOfTires(4);
+    car1.setMake("honda").setTire(4);
+    cout<<"Make : "<<car1.make<<", Tires : "<<car1.tires<<endl;
+
 }
