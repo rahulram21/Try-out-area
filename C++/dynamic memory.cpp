0cb664a81b82712ada1e3b7346d075fe41b4cff3
Rahul T V <61172472+rahulram21@users.noreply.github.com>
2024-08-12 12:24:03 +0000

    more about pointers

--- C++/dynamic memory.cpp
+++ C++/dynamic memory.cpp
@@ -5,7 +5,10 @@ int main(){
     int x = 22;
     int *p;
     p=&x;
-    cout<<x<<endl; // 22
-    cout<<*p<<endl; //22
-    cout<<p<<endl; //address
+    // cout<<x<<endl; // 22
+    // cout<<*p<<endl; //22
+    // cout<<p<<endl; //address
+    int *q = &x;
+    int y = *q;
+    cout<<y; // 22
 }
